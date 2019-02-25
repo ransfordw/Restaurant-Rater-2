370b1ecc749b1dfd527a8c5989719b90b7e3dde9
Ransford Walker <ransfordw@gmail.com>
2019-02-25 15:35:37 -0500

    Added restaurant index view

--- RestaurantRater.WebMVC/Controllers/RestaurantController.cs
+++ RestaurantRater.WebMVC/Controllers/RestaurantController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using static RestaurantRater.WebMVC.Models.Restaurant;
 
 namespace RestaurantRater.WebMVC.Controllers
 {
     public class RestaurantController : Controller
     {
+        private RestaurantDBContext db = new RestaurantDBContext();
+
         // GET: Restaurant
         public ActionResult Index()
         {
-            return View();
+            return View(db.Restaurants.ToList());
         }
     }
 }
