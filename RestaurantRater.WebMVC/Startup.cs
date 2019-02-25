07dbb1610b1ff46fc8e92d159dfb16de1b6d2087
Ransford Walker <ransfordw@gmail.com>
2019-02-25 14:04:00 -0500

    Add project files.

---
+++ RestaurantRater.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestaurantRater.WebMVC.Startup))]
+namespace RestaurantRater.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
