fac6962294823b9fa3a8089a6ed1216b87b43bca
Ransford Walker <ransfordw@gmail.com>
2019-02-26 14:23:32 -0500

    Details working and nav button for restaurant index

--- RestaurantRater.WebMVC/Controllers/RestaurantController.cs
+++ RestaurantRater.WebMVC/Controllers/RestaurantController.cs
@@ -95,6 +95,19 @@ namespace RestaurantRater.WebMVC.Controllers
             return View(restaurant);
         }
 
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            Restaurant restaurant = db.Restaurants.Find(id);
+
+            if (restaurant == null)
+                return HttpNotFound();
+
+            return View(restaurant);
+        }
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
